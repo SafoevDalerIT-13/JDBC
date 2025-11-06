c4b428d877747a42b64c5fef3b99543914b666eb
Далер <dsafoev40@gmail.com>
2025-11-06 19:21:38 +0500

    Update CRUDUtils.java

--- org.example/CRUDUtils.java
+++ org.example/CRUDUtils.java
@@ -8,6 +8,24 @@ import java.util.List;
 public class CRUDUtils {
     private static String INSERT_STUDENT = "INSERT INTO students (name, surname, curse_name) VALUES (?, ?, ?)";
     private static String GET_ALL_STUDENT = "SELECT * FROM students";
+    private static String UPDATE_STUDENT = "UPDATE students SET course_name = ? WHERE id = ?";
+    private static String DELETE_STUDENT = "DELETE FROM students WHERE id = ?";
+
+    public static List<Student> createStudentData(Student student) {
+        List<Student> students = new ArrayList<>();
+
+        try(Connection connection = DBUtils.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_STUDENT)) {
+            preparedStatement.setString(1,student.getName());
+            preparedStatement.setString(2,student.getSurname());
+            preparedStatement.setString(3,student.getCurse_name());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return students;
+    }
 
     public static List<Student> getStudentData() {
         List<Student> students = new ArrayList<>();
@@ -31,21 +49,6 @@ public class CRUDUtils {
         return students;
     }
 
-    public static List<Student> createStudentData(Student student) {
-        List<Student> students = new ArrayList<>();
-
-        try(Connection connection = DBUtils.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_STUDENT)) {
-            preparedStatement.setString(1,student.getName());
-            preparedStatement.setString(2,student.getSurname());
-            preparedStatement.setString(3,student.getCurse_name());
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return students;
-    }
 
     public static List<Student> saveStudentData(Student student) {
         List<Student> students = new ArrayList<>();
@@ -74,6 +77,51 @@ public class CRUDUtils {
         }
 
         return students;
+    }
+
+    public static List<Student> updateStudentData(int studentId, String curse_name) {
+        List<Student> updateStudents = new ArrayList<>();
+
+        try(Connection connection = DBUtils.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_STUDENT)) {
+            preparedStatement.setString(1,curse_name);
+            preparedStatement.setInt(2, studentId );
+            preparedStatement.executeUpdate();
+
+            PreparedStatement preparedStatement1 =  connection.prepareStatement(GET_ALL_STUDENT);
+            ResultSet resultSet = preparedStatement1.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                String curs_name = resultSet.getString("curse_name");
+
+                updateStudents.add(new Student(id,name,surname,curs_name));
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return updateStudents;
+
+    }
+
+    public static void deleteStudentData(int studentId) {
+
+        try(Connection connection = DBUtils.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(DELETE_STUDENT)) {
+            preparedStatement.setInt(1, studentId );
+            preparedStatement.executeUpdate();
+
+            PreparedStatement preparedStatement1 =  connection.prepareStatement(GET_ALL_STUDENT);
+            ResultSet resultSet = preparedStatement1.executeQuery();
+
+            System.out.println( getStudentData());
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
